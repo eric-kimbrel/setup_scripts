0dedeb4235d81950d81572bf1184ffd57ac3b520
Eric Kimbrel <ekimbrel@paytrace.com>
2017-05-30 09:43:18 -0700

    install under anaconda

--- install-datasketch.sh
+++ install-datasketch.sh
@@ -5,3 +5,6 @@ pip install datasketch -U
 # install for python 3.x
 sudo apt-get install -y python3-pip
 pip3 install datasketch -U
+
+# install for anaconda
+/usr/bin/anaconda/bin/pip install datasketch -U
